ea0169a4940f7221a20ac2d766e197a2f708f8fc
recep539 <89392309+recep539@users.noreply.github.com>
2022-06-13 21:24:36 +0300

    Add project files.

---
+++ ECommerce.Repository/Concrete/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using ECommerce.Core;
+using ECommerce.DAL;
+using ECommerce.DTO;
+using ECommerce.Entity;
+using ECommerce.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Repository.Concrete
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        public ProductRepository(Context db): base(db)
+        {
+
+        }
+
+        public IQueryable<ProductList> GetProductList()
+        {
+            return Set().Select(p => new ProductList
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                CategoryName = p.Category.CategoryName,
+                UnitName = p.Unit.UnitName,
+                Stock = p.Stock,
+                UnitPrice = p.UnitPrice
+            });
+        }
+
+        public IQueryable<ProductList> GetProductListByCategoryId(int id)
+        {
+            return Set().Select(p => new ProductList
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                CategoryId = p.Category.CategoryId,
+                CategoryName = p.Category.CategoryName,
+                UnitName = p.Unit.UnitName,
+                Stock = p.Stock,
+                UnitPrice = p.UnitPrice
+            }).Where(p=>p.CategoryId==id);
+        }
+    }
+}
